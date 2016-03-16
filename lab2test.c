33c9e9d4a3ce3a022082986440da1ba6b4747567
Danny rodrigues <danierodrigu@bitbucket.org>
2016-03-16 19:15:00 -0400

    testing 4

--- lab2test.c
+++ lab2test.c
@@ -11,7 +11,7 @@ main (int agrc, char *argv[]) {
 	strcpy(GOODADD,"Hello");
 	printf(1, "%s\n", GOODADD);
 
-	char* BADADD = (char*) 0x0;
+	char* BADADD = (char*) 0x3;
 	printf(1,"%s\n", BADADD);
 
 	exit();

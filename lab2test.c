c4e38f0fed61ad4ef41aa904a54dd848cba77a9a
Danny rodrigues <danierodrigu@bitbucket.org>
2016-03-16 19:04:23 -0400

    Testing good address

--- lab2test.c
+++ lab2test.c
@@ -6,6 +6,11 @@
 int
 main (int agrc, char *argv[]) {
 	printf(1, "Trying Lab 2 Test\n");
+
+	char* GOODADD = malloc(1, 10);
+
+	printf(1, "%s\n", GOODADD);
+
 	char* BADADD = (char*) 0xFFFFF0F0;
 	printf(1,"%s\n", BADADD);
 

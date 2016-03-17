5f125ab5eab0260aa7e15361d5480c8dca55e3c5
Danny rodrigues <danierodrigu@bitbucket.org>
2016-03-17 15:59:49 -0400

    Hm

--- vm.c
+++ vm.c
@@ -348,8 +348,7 @@ cowuvm(pde_t* pgdir, uint sz){
 
   pde_t *d;
   pte_t *pte;
-  uint pa, i, flags;
-  //, ref_count;
+  uint pa, i, flags, ref_count;
   
 
   if((d = setupkvm()) == 0)
@@ -369,7 +368,7 @@ cowuvm(pde_t* pgdir, uint sz){
     if(mappages(d, (void*)i, PGSIZE, pa, flags) < 0){
       panic("Error mapping");
     }
-    void* page = (void*) PGROUNDDOWN(p2v(pa));
+    void* page = (void*) PGROUNDDOWN(P2V_WO(pa));
     ref_count = getRefCount(page);
     cprintf("Read Struct run with ref count : %d\n", ref_count);
     incRefCount(page);

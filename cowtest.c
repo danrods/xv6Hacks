0156c563b41481e0b958fe1dc35e5ee4f2d71815
Danny rodrigues <danierodrigu@bitbucket.org>
2016-03-17 20:20:30 -0400

    errors too

--- cowtest.c
+++ cowtest.c
@@ -26,31 +26,31 @@ cowpatest()
   {
     pte_t *pte = walkpagedir(proc->pgdir, va, 0);
     uint add = PTE_ADDR(*pte);
-    printf(1, "Child page pysical address before: %x\n", add);
+    cprintf("Child page pysical address before: %x\n", add);
 
     //After writing
     va = "Hi";
 
-    pte_t *pte = walkpagedir(proc->pgdir, va, 0);
+    pte = walkpagedir(proc->pgdir, va, 0);
     add = PTE_ADDR(*pte);
-    printf(1, "Child page pysical address after: %x\n", add);
+    cprintf("Child page pysical address after: %x\n", add);
   }
   else if (pid > 0)
   {
     pte_t *pte = walkpagedir(proc->pgdir, va, 0);
     uint add = PTE_ADDR(*pte);
-    printf(1, "Parent page pysical address before: %x\n", add);
+    cprintf("Parent page pysical address before: %x\n", add);
 
-    int id = wait();
+    wait();
 
-    pte_t *pte = walkpagedir(proc->pgdir, va, 0);
+    pte = walkpagedir(proc->pgdir, va, 0);
     add = PTE_ADDR(*pte);
-    printf(1, "Parent page pysical address after: %x\n", add);
+    cprintf("Parent page pysical address after: %x\n", add);
     
   }
   else 
   {
-    printf(1, "Error forking");
+    cprintf("Error forking");
   }
 
 

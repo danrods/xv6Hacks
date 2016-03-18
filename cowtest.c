991c9136f7ce0d4169723010a02a9f6ecef15b64
Danny rodrigues <danierodrigu@bitbucket.org>
2016-03-17 20:28:01 -0400

    dumb

--- cowtest.c
+++ cowtest.c
@@ -26,26 +26,26 @@ cowpatest()
   //Before writing
   if (pid == 0)
   {
-    pte_t *pte = walkpagedir(proc->pgdir, va, 0);
+    pte_t *pte = (pte_t*) walkpagedir(proc->pgdir, va, 0);
     uint add = PTE_ADDR(*pte);
     cprintf("Child page pysical address before: %x\n", add);
 
     //After writing
     va = "Hi";
 
-    pte = walkpagedir(proc->pgdir, va, 0);
+    pte = (pte_t*) walkpagedir(proc->pgdir, va, 0);
     add = PTE_ADDR(*pte);
     cprintf("Child page pysical address after: %x\n", add);
   }
   else if (pid > 0)
   {
-    pte_t *pte = walkpagedir(proc->pgdir, va, 0);
+    pte_t *pte = (pte_t*) walkpagedir(proc->pgdir, va, 0);
     uint add = PTE_ADDR(*pte);
     cprintf("Parent page pysical address before: %x\n", add);
 
     wait();
 
-    pte = walkpagedir(proc->pgdir, va, 0);
+    pte = (pte_t*) walkpagedir(proc->pgdir, va, 0);
     add = PTE_ADDR(*pte);
     cprintf("Parent page pysical address after: %x\n", add);
     

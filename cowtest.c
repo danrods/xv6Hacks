ff9c01998de044d46fb383ad6eed0f14fa2188d5
Celeste <celeste.q.ma@hotmail.com>
2016-03-17 20:05:20 -0400

    adding cowtest.c

---
+++ cowtest.c
@@ -0,0 +1,61 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "defs.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+main(void)
+{
+  cowpatest();
+  exit();
+}
+
+void 
+cowpatest() 
+{
+  int pid; 
+  char* va = "Hey";
+
+  pid = fork();
+
+  //Before writing
+  if (pid == 0)
+  {
+    pte_t *pte = walkpgdir(proc->pgdir, va, 0);
+    uint add = PTE_ADDR(*pte);
+    printf("Child page pysical address before: %d\n", add);
+  }
+  else if (pid > 0)
+  {
+    pte_t *pte = walkpgdir(proc->pgdir, va, 0);
+    uint add = PTE_ADDR(*pte);
+    printf("Parent page pysical address before: %d\n", add);
+  }
+  else 
+  {
+    printf(1, "Error forking");
+  }
+
+  //After writing
+  va = "Hi";
+
+  if (pid == 0)
+  {
+    pte_t *pte = walkpgdir(proc->pgdir, va, 0);
+    uint add = PTE_ADDR(*pte);
+    printf("Child page pysical address after: %d\n", add);
+  }
+  else if (pid > 0)
+  {
+    pte_t *pte = walkpgdir(proc->pgdir, va, 0);
+    uint add = PTE_ADDR(*pte);
+    printf("Parent page pysical address after: %d\n", add);
+  }
+  else 
+  {
+    printf(1, "Error forking");
+  }
+}
+

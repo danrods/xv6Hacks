d8db3a4640a5cb8d096d8e79f79b4549881f7327
Celeste <celeste.q.ma@hotmail.com>
2016-04-09 20:07:39 -0400

    prngtest

--- prngtest.c
+++ prngtest.c
@@ -1,23 +1,17 @@
 #include "types.h"
 #include "user.h"
 
-#define RANNUMS[0xFFFFFFFF]
 #define RUN 0x48964
 
 int 
 main(int argc, char *argv[]) {
-	
+
 	for (int i = 0; i < RUN; ++i)
 	{
 		uint index = prng();
-		RANNUMS[index] += 1;
+		cprintf("%d : %d", i, index);
 	}
 
-
-	for (int i = 0; i < RUN; ++i)
-	{
-		cprintf("%d : %d", i, RANNUMS[i]);
-	}
 
 	exit();
 }

e407c63a64ebadedc5fb017927ef6b413a0b95f1
Danny rodrigues <danierodrigu@bitbucket.org>
2016-04-14 22:10:02 -0400

    turn on debugging

--- proc.c
+++ proc.c
@@ -639,8 +639,8 @@ scheduler(void)
       if(winner){ // If we found a winner
 
           if(winner->pid > 2){
-              //cprintf("Winner on CPU : %d! --> Found Ticket : { Name : %s\t Tickets : %d\t PID: %d\t Parent PID :%p \t Killed : %d \t Nice: %d\t PDIR: %p\t State: %d\n}\n", 
-              //cpu->id, winner->name, winner->tickets, winner->pid, winner->parent, winner->killed, winner->nice, winner->pgdir, winner->state);
+              cprintf("Winner on CPU : %d! --> Found Ticket : { Name : %s\t Tickets : %d\t PID: %d\t Parent PID :%p \t Killed : %d \t Nice: %d\t PDIR: %p\t State: %d\n}\n", 
+              cpu->id, winner->name, winner->tickets, winner->pid, winner->parent, winner->killed, winner->nice, winner->pgdir, winner->state);
           }
 
           // Switch to chosen process.  It is the process's job

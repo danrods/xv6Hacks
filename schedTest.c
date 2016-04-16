e626288580c52fd4c2df17a9409e9a0284870427
Danny rodrigues <danierodrigu@bitbucket.org>
2016-04-15 21:33:25 -0400

    Added computation of prime numbers

--- schedTest.c
+++ schedTest.c
@@ -130,4 +130,20 @@ int computeFib(int i, int n){
 
 	}
 
+int computePrime(int i, int n){
+
+	if(n <= 1) return 0;
+	else if(n <= 3) return 1;
+	else if(n % 2 == 0 || n % 3 == 0) return 0;
+
+	int i = 5;
+
+	while((i * i) <= n){
+		if(n % i == 0 || n % (i + 2) == 0) return 0;
+		i = i + 6;
+	}
+
+	return 1;
+}
+
 

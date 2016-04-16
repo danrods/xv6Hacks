44e7c798ef2fc7a5245e659e5555fcc6af3ca2a4
Danny rodrigues <danierodrigu@bitbucket.org>
2016-04-15 23:14:46 -0400

    Lol

--- proc.c
+++ proc.c
@@ -173,31 +173,31 @@ found:
 static
 int getTicketAmount(struct proc * proc){
 
-  if(NULL == proc) return 20; //shouldn't be null
+  if(NULL == proc) return 3; //shouldn't be null
 
-  int returnVal = 20;//By Default Return the default value for 120
+  int returnVal = 3;//By Default Return the default value for 120
 
     if(proc->nice > 139){//Too High
         cprintf("Nice value is too high!");
         proc->nice = 120;
     }
     else if(proc->nice == 139){
-        returnVal =  5;
+        returnVal =  1;
     }
     else if(proc->nice > 129){ // 130 - 138
-        returnVal =  10;
+        returnVal =  2;
     }
     else if(proc->nice > 119){ // 120 - 129
-        returnVal =  20;
+        returnVal =  3;
     } 
     else if(proc->nice > 109){ // 110 - 119
-        returnVal =  30;
+        returnVal =  5;
     }
     else if(proc->nice > 100){ // 101 - 109
-        returnVal =  40;
+        returnVal =  10;
     }
     else if(proc->nice == 100){ 
-        returnVal =  50;
+        returnVal =  15;
     }
     else{ // Too Low
         cprintf("Nice value is too low!");

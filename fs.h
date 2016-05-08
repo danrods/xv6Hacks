12392aa5aa80a7f3956c7f5e34055c542b8f6a3c
Danny rodrigues <danierodrigu@bitbucket.org>
2016-05-08 16:58:48 -0400

    hm

--- fs.h
+++ fs.h
@@ -176,19 +176,19 @@ struct dirent {
 
 #define func_enter() cprintf("=========== File: %s | Function: %s | Line number: %d===========\n", __FILE__, __func__, __LINE__)
 
-#define func_exit(fmt, ...) cprintf("Returning Args: \n");cprintf(fmt, ##__VA_ARGS__); cprintf("{{{{{{{{{{{{{{{{{{{{{{ END Function: %s }}}}}}}}}}}}}}}}}}}}}}}}\n", __func__)
+#define func_exit(fmt, ...) cprintf("Returning Args: \n");cprintf(fmt, ##__VA_ARGS__); cprintf("{{{{{{{{{{{{{{{{{{{{{{ END Function: %s }}}}}}}}}}}}}}}}}}}}}}}}\n\n", __func__)
 
 #define fs_debug(fmt, ...) cprintf("[Debug] --->"); cprintf(fmt, ##__VA_ARGS__)
 #define fs_error(fmt, ...) cprintf("~~~~~~~[Error] --->"); cprintf(fmt, ##__VA_ARGS__)
 #define iNode_info(i) fs_debug("iNode : {Dev: %d, iNum: %d, Ref Count: %d, Flags: %d, Size: %d}\n", i->dev, i->inum, i->ref, i->flags, i->size)
 #define buf_info(b) fs_debug("Buf : {Dev: %d, BlockNo: %d, Flags: %d, Prev: %d, Next: %d}\n", b->dev, b->blockno, b->flags, b->prev->blockno, b->next->blockno)
 #define diNode_info(d) do{                                                                                                                      \
-                          cprintf("{Type: %d, Major: %d, Minor: %d, Link: %d, Size: %d \t[", d->type, d->major, d->minor, d->nlink, d->size); int i;\
+                          cprintf("{Type: %d, Major: %d, Minor: %d, Link: %d, Size: %d, Addrs[", d->type, d->major, d->minor, d->nlink, d->size); int i;\
                           for(i=0; i < NDIRECT; i++){                                                                                           \
                                if(d->addrs[i]) cprintf("Direct Address %d => %d,", d->addrs[i]);                                                                \
                           }                                                                                                                     \
                           if(d->addrs[NDIRECT]) cprintf("Indirect Address => %d\n", d->addrs[NDIRECT]);                                           \
-                          cprintf("]\n");                                                                                                       \
+                          cprintf("] }\n");                                                                                                       \
                           }while(0)                                                                                                       
 
 

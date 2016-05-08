2da8c8d1a769f9d911f40b5dcb356e6128408208
Celeste <celeste.q.ma@hotmail.com>
2016-05-07 21:17:34 -0400

    test file

---
+++ lab4tests.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "user.h"
+
+
+
+
+int
+main(void) {
+	
+
+	exit();
+}
+
+
+// Test a new directory is placed in one of the least
+// utilized block groups
+void dirTest() {
+	
+}

cda6be2a9f4ed3659f0300b1de48c07d517cf07c
Celeste <celeste.q.ma@hotmail.com>
2016-05-09 11:11:38 -0400

    tests

--- lab4tests.c
+++ lab4tests.c
@@ -5,26 +5,52 @@ int
 main(void) {
 	static const char DIRNAME = "newDir";
 	static const char FILENAME = "newFile";
+	static const char LARGEFILE = "LargeFile";
+	static const int SIZE = 128000;	 // 16KB
 
-
+	// First Heuristic Test
 	printf("Test I: a new directory placement");
 	printBStats();
 
-	int file = mkdir(*DIRNAME);
+	int path = createDir(*DIRNAME);
 
 	// Not created
-	if (file == -1)
+	if (path == -1)
 	{
-		printf("ERROR: Failed to create the file");
+		printf("ERROR: Failed to create the directory");
 		exit();
 	}
 	else {
 		printBStats();
 	}
 
+	// Second Heuristic Test
+	printf("Test II: a new file placement");
 
-	printf("Test II: a new file placement")
-	
+	int file = createFile(*FILENAME);
+	if (file == -1)
+	{
+		printf("ERROR: Failed to create the file");
+		exit();
+	}
+	else 
+	{
+		printBStats();
+	}
+
+
+	// Third Heuristic Test
+	printf("Test II: a new file placement");
+	int bigFile = createFile(*FILENAME, SIZE);
+	if (bigFile == -1)
+	{
+		printf("ERROR: Failed to create the big file");
+		exit();
+	}
+	else {
+		printBStats();
+	}
+
 
 	exit();
 }
